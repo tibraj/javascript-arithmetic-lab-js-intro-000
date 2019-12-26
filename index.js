da2b30e94963b29ad3d9c1e30314a66b99e2745d
Taulant Ibraj <tibraj94@gmail.com>
2019-12-26 17:00:44 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function add(){
+return a+b;
+}
+add(a, b);
+function subtract(){
+  return a-b;
+  }
+  subtract(a, b);
+function multiply(){
+  return a*b;
+}
+ multiply(a,b);
+function divide (){
+  return a/b;
+}
+divide(a,b);
+function inc(n){
+  return n+=1;
+}
+function dec(n){
+  return n-=1;
+}
+function makeInt(n){
+  return parseInt(n,10);
+}
+function preserveDecimal(n){
+  return parseFloat(n);
+}
